1d83e7f8d12ba9c5dcd93a0cd3eecd0c8145f247
Allan Jardine <allan@datatables.net>
2022-10-10 19:48:53 +0000

    Add support for mjs files (ES Modules)

---
+++ js/colReorder.dataTables.mjs
@@ -0,0 +1,13 @@
+
+/*! DataTables styling wrapper for ColReorder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-dt';
+import DataTable from 'datatables.net-colreorder';
+
+
+
+
+export default DataTable;
