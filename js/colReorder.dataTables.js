57a369e7193cfaf72cea0e22980433506369c5b1
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:09:43 +0000

    b03ec4b81a6e3d607437a6c23649032b63740d17
    Sync to source repo @03ec4b81a6e3d607437a6c23649032b63740d17

--- js/colReorder.dataTables.js
+++ js/colReorder.dataTables.js
@@ -28,10 +28,9 @@
 				require('datatables.net-dt')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.ColReorder ) {
 				require('datatables.net-colreorder')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/colReorder.dataTables.min.mjs
+++ js/colReorder.dataTables.min.mjs
@@ -1,4 +1,4 @@
 /*! DataTables styling wrapper for ColReorder
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-dt";import"datatables.net-colreorder";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-dt";import ColReorder from"datatables.net-colreorder";export default DataTable;
