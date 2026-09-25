4b35c671d7a61629ae1f32476d61ef659f582a6e
dtbuild <dtbuild@rigil>
2026-09-25 11:10:47 +0000

    c822e7a9895877fc49c1788356eb1156ae7b3a8f Release 3.1.2
    Sync to source repo @c822e7a9895877fc49c1788356eb1156ae7b3a8f

--- js/colReorder.dataTables.min.mjs
+++ js/colReorder.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! ColReorder default styling 3.1.1 for DataTables
+/*! ColReorder default styling 3.1.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import ColReorder from"datatables.net-colreorder";export default DataTable;
